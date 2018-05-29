a652b0814b8ad5bb78cf870b0bbf73266d5af3d3
Abduvokhid <wahid@sju.ac.kr>
2018-05-29 09:59:46 +0900

    Initial Commit

---
+++ OpenGL Project#3 (Lighting Effects)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL Project#3 (Lighting Effects)
+//
+//  Created by mac on 5/29/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
