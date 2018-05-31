29663be5e5ad81c84565e789ad2d1077cb82b240
Abduvokhid <wahid@sju.ac.kr>
2018-05-31 17:20:20 +0900

    Texture mapping class code

--- OpenGL Project#3 (Lighting Effects)/main.cpp
+++ OpenGL Project#3 (Lighting Effects)/main.cpp
@@ -24,121 +24,94 @@
 
 #include <iostream>
 
-#define MAXZ 8.0
-#define MINZ -8.0
-#define ZINC 0.4
-static float solidZ = MAXZ;
-static float transparentZ = MINZ;
+GLfloat mat_diffuse[] = { 0.25, 0.25, 1., 0. };
+GLfloat mat_specular[] = { 1., 1., 1., 0. };
+GLfloat light_position[] = { 10., 10., 20., 1. };
+GLfloat ref_plane[] = { 1.5, 1.5, 1.5, 0. };
+GLUquadricObj* qobj;
+unsigned int MyTextureObject;
 
-static GLuint sphereList, lightList;
-static int spin = 0;
+#define SImageWidth 32
 
-void init(void)
+GLubyte SImage[4*SImageWidth];
+
+
+void MyStripeImage()
 {
-    glClearColor (0.2,0.2,0.2, 0.0); // slightly dark, not black
-    GLfloat light_ambient[] = { 0.133, 0.545, 0.133, 1.0 }; //forest green
-    GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat light_specular[] = { 1.0, .0, .0, 1.0 };
-    GLfloat light_position[] = { 1.0, 1.0, 1.0, 1.0 };
+    for(int i=0;i<SImageWidth;i++)
+    {
+        SImage[4*i]=255;
+        SImage[4*i+1]=(i<8)?0:255;
+        SImage[4*i+2]=(i<8)?0:255;
+        SImage[4*i+3]=0;
+    }
     
-    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-    
-    
-    glShadeModel (GL_SMOOTH);
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-    glEnable(GL_DEPTH_TEST);
-    
-    sphereList = glGenLists(1);
-    glNewList(sphereList, GL_COMPILE);
-    glutSolidSphere (1.0, 300, 16);
-    glEndList();
-    
-    lightList = glGenLists(1);
-    glNewList(lightList, GL_COMPILE);
-    glutSolidSphere (.05, 300, 16);
-    glEndList();
-
     
 }
 
-
-void display(void)
-{
-    
-    GLfloat mat_solid[] = { 0.75, 0.075, 0.0, 1.0 };
-    GLfloat mat_zero[] = { .0, .0, .0, 1.0 };
-    GLfloat mat_transparent[] = { 0.6, 0.6, 0.6, 0.8 };
-    GLfloat mat_emission[] = { 0.0, 0.3, 0.3, 0.6 };
-    
-    
-    GLfloat position[] = { 0.0, 0.0, 1.5, 1.0 };
-    
-    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glPushMatrix ();
-    glTranslatef (0.0, 0.0, -5.0);
-    
-    glPushMatrix ();
-    glRotated ((GLdouble) spin, 1.0, 0.0, 0.0);
-    glLightfv (GL_LIGHT0, GL_POSITION, position);
-    
-    glTranslated (0.0, 0.0, 1.5);
-    glDisable (GL_LIGHTING);
-    glColor3f (1.0, 1.0, 1.0);
-    glMaterialfv(GL_FRONT, GL_EMISSION, mat_zero);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_solid);
-    glCallList (lightList);
-    glEnable (GL_LIGHTING);
-    glPopMatrix ();
-    
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_transparent);
-    glEnable (GL_BLEND);
-    glDepthMask (GL_FALSE);
-    glBlendFunc (GL_SRC_ALPHA, GL_SRC_ALPHA);
-    glCallList (sphereList);
-    glDepthMask (GL_TRUE);
-    glDisable (GL_BLEND);
-    glPopMatrix ();
+void MyDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBindTexture(GL_TEXTURE_1D, MyTextureObject);
+    gluSphere(qobj, 1.5, 40, 40);
     glutSwapBuffers();
 }
 
-void reshape (int w, int h)
-{
-    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
-    glMatrixMode (GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(40.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+void Init() {
+    qobj = gluNewQuadric();
+    gluQuadricDrawStyle(qobj, GLU_FILL);
+    
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialf(GL_FRONT, GL_SHININESS, 25.0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_DEPTH_TEST);
+    glShadeModel(GL_SMOOTH);
+    
+    MyStripeImage();
+    glGenTextures(1, &MyTextureObject); // generate texture
+    glBindTexture(GL_TEXTURE_1D, MyTextureObject); // bind
+    glTexImage1D(GL_TEXTURE_1D, 0, 4, SImageWidth, 0, GL_RGBA, GL_UNSIGNED_BYTE, SImage); // only level 0 exists
+    
+    glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+    glTexGenfv(GL_S, GL_OBJECT_PLANE, ref_plane);
+    
+    // Wrapping
+    glTexParameterf(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    
+    // filtering
+    glTexParameterf(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    glTexParameterf(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+    glEnable(GL_TEXTURE_GEN_S);
+    glEnable(GL_TEXTURE_1D);
+    
+
+    
 }
 
-void mouse(int button, int state, int x, int y)
-{
-    switch (button) {
-        case GLUT_LEFT_BUTTON:
-            if (state == GLUT_DOWN) {
-                spin = (spin + 30) % 360;
-                glutPostRedisplay();
-            }
-            break;
-        default:
-            break;
-    }
+void MyReshape(int w, int h) {
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(40., (GLfloat)w / (GLfloat)h, 1., 10.);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0., 0., 5., 0., 0., 0., 0., 1., 0.);
+    glTranslatef(0., 0., -1.);
 }
-int main(int argc, char** argv)
-{
+
+int main(int argc, char **argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize (500, 500);
-    glutInitWindowPosition (100, 100);
-    glutCreateWindow (argv[0]);
-    init ();
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutMouseFunc(mouse);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutCreateWindow("openGL sample program");
+    glutReshapeFunc(MyReshape);
+    glutDisplayFunc(MyDisplay);
+    Init();
     glutMainLoop();
+    
     return 0;
 }
